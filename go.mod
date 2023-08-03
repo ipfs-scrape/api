9edd9f34e8265345aff7093f8bf30c4cad20a120
jothflee <62022524+jothflee@users.noreply.github.com>
2023-08-03 17:09:19 -0400

    chore: init

---
+++ go.mod
@@ -0,0 +1,76 @@
+module github.com/ipfs-scrape/api
+
+go 1.19
+
+require (
+	github.com/aws/aws-sdk-go v1.44.316
+	github.com/google/uuid v1.3.0
+	github.com/sirupsen/logrus v1.9.3
+)
+
+require (
+	github.com/Knetic/govaluate v3.0.1-0.20171022003610-9aa49832a739+incompatible // indirect
+	github.com/beevik/etree v1.2.0 // indirect
+	github.com/bytedance/sonic v1.10.0-rc3 // indirect
+	github.com/casbin/casbin/v2 v2.73.0 // indirect
+	github.com/chenzhuoyu/base64x v0.0.0-20230717121745-296ad89f973d // indirect
+	github.com/chenzhuoyu/iasm v0.9.0 // indirect
+	github.com/crewjam/httperr v0.2.0 // indirect
+	github.com/crewjam/saml v0.4.13 // indirect
+	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.2.0 // indirect
+	github.com/foolin/gin-template v0.0.0-20190415034731-41efedfb393b // indirect
+	github.com/gabriel-vasile/mimetype v1.4.2 // indirect
+	github.com/gin-contrib/cors v1.4.0 // indirect
+	github.com/gin-contrib/gzip v0.0.6 // indirect
+	github.com/gin-contrib/location v0.0.2 // indirect
+	github.com/gin-contrib/requestid v0.0.6 // indirect
+	github.com/gin-contrib/sessions v0.0.5 // indirect
+	github.com/gin-contrib/sse v0.1.0 // indirect
+	github.com/gin-gonic/gin v1.9.1 // indirect
+	github.com/go-playground/locales v0.14.1 // indirect
+	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/go-playground/validator/v10 v10.14.1 // indirect
+	github.com/goccy/go-json v0.10.2 // indirect
+	github.com/gokitcloud/ginkit v0.9.0 // indirect
+	github.com/golang-jwt/jwt/v4 v4.5.0 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/gorilla/context v1.1.1 // indirect
+	github.com/gorilla/securecookie v1.1.1 // indirect
+	github.com/gorilla/sessions v1.2.1 // indirect
+	github.com/gwatts/gin-adapter v1.0.0 // indirect
+	github.com/ipfs-scrape/worker v0.0.0-20230803205901-3ff0dbec9256 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/jonboulle/clockwork v0.4.0 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/klauspost/cpuid/v2 v2.2.5 // indirect
+	github.com/leodido/go-urn v1.2.4 // indirect
+	github.com/lestrrat-go/backoff/v2 v2.0.8 // indirect
+	github.com/lestrrat-go/blackmagic v1.0.1 // indirect
+	github.com/lestrrat-go/httpcc v1.0.1 // indirect
+	github.com/lestrrat-go/iter v1.0.2 // indirect
+	github.com/lestrrat-go/jwx v1.2.26 // indirect
+	github.com/lestrrat-go/option v1.0.1 // indirect
+	github.com/mattermost/xml-roundtrip-validator v0.1.0 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/pelletier/go-toml/v2 v2.0.9 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/quasoft/memstore v0.0.0-20191010062613-2bce066d2b0b // indirect
+	github.com/russellhaering/goxmldsig v1.4.0 // indirect
+	github.com/tidwall/gjson v1.15.0 // indirect
+	github.com/tidwall/match v1.1.1 // indirect
+	github.com/tidwall/pretty v1.2.1 // indirect
+	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
+	github.com/ugorji/go/codec v1.2.11 // indirect
+	golang.org/x/arch v0.4.0 // indirect
+	golang.org/x/crypto v0.11.0 // indirect
+	golang.org/x/net v0.13.0 // indirect
+	golang.org/x/oauth2 v0.10.0 // indirect
+	golang.org/x/sys v0.10.0 // indirect
+	golang.org/x/text v0.11.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,159 @@
+package main
+
+import (
+	"encoding/csv"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/gin-gonic/gin"
+	"github.com/gokitcloud/ginkit"
+	"github.com/ipfs-scrape/worker/backend"
+	"github.com/ipfs-scrape/worker/ipfs"
+	"github.com/ipfs-scrape/worker/queue"
+	"github.com/sirupsen/logrus"
+)
+
+func main() {
+	dynamodbName, ok := os.LookupEnv("IPFS_DYNAMODB_NAME")
+	if !ok {
+		logrus.Fatal("IPFS_DYNAMODB_NAME environment variable not set")
+	}
+
+	// Use the IPFS_DYNAMODB_NAME environment variable
+	logrus.Infof("IPFS_DYNAMODB_NAME: %s\n", dynamodbName)
+
+	sess, err := session.NewSession()
+	if err != nil {
+		logrus.Fatal(err)
+	}
+
+	svc := dynamodb.New(sess)
+	dynamodbBackend, err := backend.NewDynamoDBBackend(dynamodbName, svc)
+	if err != nil {
+		logrus.Fatal(err)
+	}
+
+	dynamodbQueue, err := queue.NewDynamoDBQueue(dynamodbName, "ipfs", svc)
+	if err != nil {
+		logrus.Fatal(err)
+	}
+
+	r := ginkit.Default()
+	r.GET("/ping", "pong")
+	r.GET("/tokens", func(p ginkit.Params) (any, error) {
+		items, err := dynamodbBackend.Scan("d-")
+		logrus.Info("retrieved full dataset")
+		return items, err
+	})
+	r.GET("/tokens/:cid", func(p ginkit.Params) (any, error) {
+		if cid, ok := p.Get("cid"); ok {
+			logrus.WithField("cid", cid).Info("cid")
+			item, err := dynamodbBackend.Read(ipfs.GenerateIDFromCID(cid))
+			if err != nil {
+				logrus.Error(err)
+				return nil, err
+			}
+			return item, nil
+		}
+		return nil, errors.New("invalid")
+	})
+
+	r.POST("/tokens/:cid", func(p ginkit.Params) (any, error) {
+		if cid, ok := p.Get("cid"); ok {
+			err := dynamodbQueue.AddItem(queue.NewQueueItem(cid, map[string]any{
+				"cids": []string{cid},
+			}))
+			return "OK", err
+		}
+		return nil, errors.New("invalid")
+	})
+
+	r.POST("/bulk", func(c *gin.Context) {
+		var data struct {
+			Cids []string `json:"cids"`
+		}
+
+		if err := c.ShouldBindJSON(&data); err != nil {
+			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "invalid request body"})
+			return
+		}
+
+		if len(data.Cids) == 0 {
+			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "missing cids"})
+			return
+		}
+
+		err := dynamodbQueue.AddItem(queue.NewQueueItem(fmt.Sprintf("bulk-%v", time.Now().UnixNano()), map[string]interface{}{
+			"cids": data.Cids,
+		}))
+
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": "failed to add item to queue"})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{"message": "OK"})
+
+	})
+	r.POST("/csv", func(c *gin.Context) {
+		file, _, err := c.Request.FormFile("file")
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "missing file"})
+			return
+		}
+		defer file.Close()
+
+		reader := csv.NewReader(file)
+		reader.FieldsPerRecord = 1
+
+		var cids []string
+		for {
+			record, err := reader.Read()
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "invalid CSV file"})
+				return
+			}
+			cids = append(cids, record[0])
+
+			if len(cids) >= 5 {
+				err = dynamodbQueue.AddItem(queue.NewQueueItem(fmt.Sprintf("csv-%v", time.Now().UnixNano()), map[string]interface{}{
+					"cids": cids,
+				}))
+
+				if err != nil {
+					c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": "failed to add item to queue"})
+					return
+				}
+				cids = []string{}
+			}
+		}
+
+		if len(cids) == 0 {
+			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "missing cids"})
+			return
+		}
+
+		err = dynamodbQueue.AddItem(queue.NewQueueItem(fmt.Sprintf("csv-%v", time.Now().UnixNano()), map[string]interface{}{
+			"cids": cids,
+		}))
+
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": "failed to add item to queue"})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{"message": "OK"})
+	})
+
+	r.Run() // listen and serve on 0.0.0.0:8080
+
+}
